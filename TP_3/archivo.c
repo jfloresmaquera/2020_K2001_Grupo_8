e9fc1defa0c245f41b5a5d51591b196b505f4626
Mecha Magnelli <mechamagnelli@gmail.com>
2020-08-02 21:31:26 -0300

    Falta arreglar los identificadores.
    Co-authored-by: rlarrart <rodrigolarrart@gmail.com>
    Co-authored-by: Ckanerloy <camilakanerloy@gmail.com>
    Co-authored-by: Nickbenve <benve.nicolas@gmail.com>
    Co-authored-by: Juan Alberto Flores Maquera <jfloresm96@gmail.com>

---
+++ TP_3/archivo.c
@@ -0,0 +1,25 @@
+// hola
+// como estas?
+//holaaaaaaaaaaaa
+/* jeje 
+*/
+
+int a;
+a; 
+bA; x4
+_holaaaaa
+'c'
+'c'
+'?'
+"hola"
+ab;
+return; 
+ab; ab;
+a;
+_holaaaaa
+bc
+
+bababa
+momomo
+_hola 
+
